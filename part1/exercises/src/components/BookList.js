9c7565111231274a61dbd4db97f456b6da69c0fd
Carter <carte@CG>
2023-08-07 12:57:26 -0500

    Finished Part 1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books";
+   let book1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFBXQk4-L2N3PdLjxy6pMFt_YyyDm94wLmsw&usqp=CAU";
+   let book2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQkVp4B1VW4iS-1UIqMhyY8iUgnrXRHaVMEw&usqp=CAU";
+   let book3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR324MAGr_dmk8YtL4QxE1mmHSHJJwyoFRsZQ&usqp=CAU";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Horton Hears a Who" />
+         <img src={book2} alt="The Grinch" />
+         <img src={book3} alt="The Butter Battle Book" />
       </div>      
    );
 }
